863f9788dcca02c2be0448364474cac8fa4fcd7a
Nick Adams <nadams@qa.com>
2021-02-09 16:00:43 +0000

    git commit -m

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,27 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+
+CREATE TABLE IF NOT EXISTS `orderitems` (
+`Order_Items_ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`order_ID` INT NOT NULL,
+`product_ID` INT NOT NULL,
+`Quantity` INT NOT NULL,
+FOREIGN KEY (`order_ID`) REFERENCES orders(`order_ID`),
+FOREIGN KEY (`product_ID`) REFERENCES items(`product_ID`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `items` (
+`product_ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`Product_name` VARCHAR(50) NOT NULL,
+`Product_price` DECIMAL(10,2) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS `orders` (
+`order_ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`id` INT NOT NULL,
+FOREIGN KEY (`id`) REFERENCES customers(`id`)
+);
--- src/test/java/com/qa/ims/controllers/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -30,15 +30,59 @@ public class ItemControllerTest {
 	
 	@Test
 	public void testCreate() {
-		final String F_NAME = "barry", L_NAME = "scott";
-		final Customer created = new Customer(F_NAME, L_NAME);
+		final String productName = "Sock";
+		final Double productPrice = 1.99;
+		final Item created = new Item(productName, productPrice);
 
-		Mockito.when(utils.getString()).thenReturn(F_NAME, L_NAME);
+		Mockito.when(utils.getString()).thenReturn(productName);
+		Mockito.when(utils.getDouble()).thenReturn(productPrice);
 		Mockito.when(dao.create(created)).thenReturn(created);
 
 		assertEquals(created, controller.create());
 
-		Mockito.verify(utils, Mockito.times(2)).getString();
+		Mockito.verify(utils, Mockito.times(1)).getString();
+		Mockito.verify(utils, Mockito.times(1)).getDouble();
 		Mockito.verify(dao, Mockito.times(1)).create(created);
+	} 
+	
+	@Test
+	public void testReadAll() {
+		List<Item> items = new ArrayList<>();
+		customers.add(new Customer(1L, "jordan", "harrison"));
+ 
+		Mockito.when(dao.readAll()).thenReturn(customers);
+
+		assertEquals(customers, controller.readAll());
+
+		Mockito.verify(dao, Mockito.times(1)).readAll();
 	}
+
+	@Test
+	public void testUpdate() {
+		Customer updated = new Customer(1L, "chris", "perrins");
+
+		Mockito.when(this.utils.getLong()).thenReturn(1L);
+		Mockito.when(this.utils.getString()).thenReturn(updated.getFirstName(), updated.getSurname());
+		Mockito.when(this.dao.update(updated)).thenReturn(updated);
+
+		assertEquals(updated, this.controller.update());
+
+		Mockito.verify(this.utils, Mockito.times(1)).getLong();
+		Mockito.verify(this.utils, Mockito.times(2)).getString();
+		Mockito.verify(this.dao, Mockito.times(1)).update(updated);
+	}
+
+	@Test
+	public void testDelete() {
+		final long ID = 1L;
+
+		Mockito.when(utils.getLong()).thenReturn(ID);
+		Mockito.when(dao.delete(ID)).thenReturn(1);
+
+		assertEquals(1L, this.controller.delete());
+
+		Mockito.verify(utils, Mockito.times(1)).getLong();
+		Mockito.verify(dao, Mockito.times(1)).delete(ID);
+	}
+
 }
