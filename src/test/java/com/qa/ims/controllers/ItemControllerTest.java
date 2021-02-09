8c024d08cc10cd8c9a132c32aa43e796915d5e66
Nick Adams <nadams@qa.com>
2021-02-09 16:19:35 +0000

    IM-41 #comment Item Controller tests

--- src/test/java/com/qa/ims/controllers/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.controller.ItemController;
 import com.qa.ims.persistence.dao.ItemDAO;
-import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.Utils;
 
@@ -48,27 +47,29 @@ public class ItemControllerTest {
 	@Test
 	public void testReadAll() {
 		List<Item> items = new ArrayList<>();
-		customers.add(new Customer(1L, "jordan", "harrison"));
+		items.add(new Item(1L, 1.99, "Sock"));
  
-		Mockito.when(dao.readAll()).thenReturn(customers);
+		Mockito.when(dao.readAll()).thenReturn(items);
 
-		assertEquals(customers, controller.readAll());
+		assertEquals(items, controller.readAll());
 
 		Mockito.verify(dao, Mockito.times(1)).readAll();
 	}
 
 	@Test
 	public void testUpdate() {
-		Customer updated = new Customer(1L, "chris", "perrins");
+		Item updated = new Item(1L, 1.99, "Sock");
 
 		Mockito.when(this.utils.getLong()).thenReturn(1L);
-		Mockito.when(this.utils.getString()).thenReturn(updated.getFirstName(), updated.getSurname());
+		Mockito.when(this.utils.getDouble()).thenReturn(updated.getProductPrice());
+		Mockito.when(this.utils.getString()).thenReturn(updated.getProductName());
 		Mockito.when(this.dao.update(updated)).thenReturn(updated);
+		//Mockito.when(this.utils.getString()).thenReturn(updated.getFirstName(), updated.getSurname());
 
 		assertEquals(updated, this.controller.update());
 
 		Mockito.verify(this.utils, Mockito.times(1)).getLong();
-		Mockito.verify(this.utils, Mockito.times(2)).getString();
+		Mockito.verify(this.utils, Mockito.times(1)).getString();
 		Mockito.verify(this.dao, Mockito.times(1)).update(updated);
 	}
 
